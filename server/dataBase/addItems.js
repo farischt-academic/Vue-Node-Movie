ad5e5da72412504ce13e3e03a94cbbe925fbb7f7
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-15 22:11:23 +0100

    added movies to db

--- server/dataBase/addItems.js
+++ server/dataBase/addItems.js
@@ -1,32 +1,111 @@
-const { client } = require("./index");
+const { Client } = require("pg");
+
+client = new Client({
+  user: "postgres", //process.env.DB_USER,
+  host: "localhost", //process.env.DB_HOST,
+  password: "lolilollpb", //process.env.DB_PASSWORD,
+  database: "PROJETWEB", //process.env.DB_NAME,
+});
 
 client.connect();
 
 const movies = [
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
-  { name: "", duration: "", realisator: "", summary: "", url: "", rating: 0 },
+  {
+    title: "Inception",
+    description:
+      "Dom Cobb est un voleur expérimenté – le meilleur qui soit dans l’art périlleux de l’extraction : sa spécialité consiste à s’approprier les secrets les plus précieux d’un individu, enfouis au plus profond de son subconscient, pendant qu’il rêve et que son esprit est particulièrement vulnérable. Très recherché pour ses talents dans l’univers trouble de l’espionnage industriel, Cobb est aussi devenu un fugitif traqué dans le monde entier qui a perdu tout ce qui lui est cher.",
+    rating: 0,
+    cast: ["Leonardo Dicaprio", "Marion Cotillard", "Ellen Page"],
+    url:
+      "https://images-na.ssl-images-amazon.com/images/I/81CgNB2mglL._SL1425_.jpg",
+    duration: "2h28 min",
+    realisator: "Christopher Nolan",
+  },
+  {
+    title: "Interstellar",
+    description:
+      "Le film raconte les aventures d’un groupe d’explorateurs qui utilisent une faille récemment découverte dans l’espace-temps afin de repousser les limites humaines et partir à la conquête des distances astronomiques dans un voyage interstellaire. ",
+    rating: 0,
+    cast: [
+      "Matthew McConaughey",
+      "Anne Hathaway",
+      "Michael Caine",
+      "Michael Caine",
+    ],
+    url:
+      "https://media.senscritique.com/media/000018762465/source_big/Interstellar.jpg",
+    duration: "2h49 min",
+    realisator: "Christopher Nolan",
+  },
+  /*{
+    title: "",
+    description: "",
+    rating: 0,
+    cast: [""],
+    url: "",
+    duration: "",
+    realisator: "",
+  },
+  {
+    title: "",
+    description: "",
+    rating: 0,
+    cast: [""],
+    url: "",
+    duration: "",
+    realisator: "",
+  },
+  {
+    title: "",
+    description: "",
+    rating: 0,
+    cast: [""],
+    url: "",
+    duration: "",
+    realisator: "",
+  },
+  {
+    title: "",
+    description: "",
+    rating: 0,
+    cast: [""],
+    url: "",
+    duration: "",
+    realisator: "",
+  },
+  {
+    title: "",
+    description: "",
+    rating: 0,
+    cast: [""],
+    url: "",
+    duration: "",
+    realisator: "",
+  },
+  {
+    title: "",
+    description: "",
+    rating: 0,
+    cast: [""],
+    url: "",
+    duration: "",
+    realisator: "",
+  },*/
 ];
 
 movies.forEach(async (movie) => {
-  await client.query({
+  const sql = await client.query({
     text:
-      "INSERT INTO movies (name, duration, realisator, summary, url, rating) VALUES ($1, $2, $3, $4, $5)",
+      "INSERT INTO movies (title, description, rating, url, duration, realisator) VALUES ($1, $2, $3, $4, $5, $6)",
     values: [
-      movie.name,
+      movie.title,
+      movie.description,
+      movie.rating,
+      // movie.cast,
+      movie.url,
       movie.duration,
       movie.realisator,
-      movie.summary,
-      movie.url,
-      movie.rating,
     ],
   });
+  console.log(sql);
 });
