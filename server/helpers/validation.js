e771f2abb6439e1d53788c3568c27a32400ca97a
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-16 21:38:56 +0100

    ?

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -21,7 +21,6 @@ const passwordValidation = (password, repeatedPassword) => {
 const loginValidation = (data) => {
   const JoiSchema = Joi.object({
     email: Joi.string().min(6).required().email(),
-
     password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
   });
   return JoiSchema.validate(data);
