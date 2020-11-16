8b525d60cb4070226fb35bf59a9818e1a45927e3
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-16 21:39:22 +0100

    routes for create and update movie

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { listMovies, readMovie } = require("../controllers/movieController");
+const { verifyAuth } = require("../helpers/verifyAuth");
+
+const {
+  listMovies,
+  readMovie,
+  createMovie,
+} = require("../controllers/movieController");
 
 // Route that list all the movies
 router.get("/", listMovies);
 // Route that read a specific movie using the id
 router.get("/:movieId", readMovie);
+// Route that create a movie
+router.post("/", verifyAuth, createMovie);
+// Route that update a movie
 
 module.exports = router;
