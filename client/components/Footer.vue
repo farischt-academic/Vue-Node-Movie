f323e996e705c486bb6cb35e9d188fe098fd518e
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-16 21:33:01 +0100

    bug fixed, footer is now in a good position

--- client/components/Footer.vue
+++ client/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+  <footer class="bg-dark">
     <div class="container-fluid">
       <div class="row">
         <div class="col-12 col-md">
@@ -46,6 +46,8 @@ module.exports = {};
 
 <style scoped>
 footer {
-  background-color: white;
+  color: white;
+  padding: 40px;
+  margin-top: 10px;
 }
 </style>
