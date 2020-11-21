8d97e51e11eb8690f903e131286b827b00cc6bb7
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-21 01:40:55 +0100

    new data and computed in order to fix pagination

--- client/components/MovieContainer.vue
+++ client/components/MovieContainer.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="album py-3">
-    <h1 class="text-center display-4">Notre selection</h1>
+    <h1 class="text-center display-4">Notre sélection</h1>
     <div class="row m-2">
       <div v-for="movie in movies" :key="movie.id" class="col-md-4">
-        <movie-card :movie="movie"> </movie-card>
+        <movie-card :movie="movie" :liked="liked"> </movie-card>
       </div>
     </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li v-for="index in numberOfPages" :key="index" class="page-item">
+          <a class="page-link" href="#">{{ index }}</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -16,13 +23,23 @@ module.exports = {
   components: { "movie-card": MovieCard },
   props: {
     movies: { type: Array, default: [] },
+    liked: { type: Array, default: [] },
+  },
+  data() {
+    return {
+      moviesPerPage: 1,
+    };
+  },
+  computed: {
+    numberOfPages: function () {
+      return this.movies.length / this.moviesPerPage;
+    },
   },
 };
 </script>
 
 <style scoped>
 .album {
-  background-color: #e8ecef;
   border-radius: 5px;
   color: black;
   margin-top: 50px;
