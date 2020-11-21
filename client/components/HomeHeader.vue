1829e9e2122b6613ec83685eeffb02b09f7e1b39
Faris Chtatou <f.chtatou@hotmail.fr>
2020-11-21 01:39:01 +0100

    brut text

--- client/components/HomeHeader.vue
+++ client/components/HomeHeader.vue
@@ -2,9 +2,12 @@
   <div class="jumbotron">
     <h1 class="display-4">Bienvenue sur Ciné+</h1>
     <p>
-      This is a template for a simple marketing or informational website. It
-      includes a large callout called a jumbotron and three supporting pieces of
-      content. Use it as a starting point to create something more unique.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aspernatur
+      est sed. Vero, blanditiis itaque. Pariatur quisquam ab repudiandae libero.
+      Illo placeat laudantium vero est ducimus explicabo consectetur, tenetur
+      nostrum quod. Doloremque corrupti, veniam totam architecto ea voluptate at
+      dolore dolorem temporibus debitis, neque deserunt sapiente rerum suscipit
+      labore hic!
     </p>
     <p>
       <a class="btn btn-info btn-lg" href="#" role="button"
@@ -19,4 +22,11 @@ module.exports = {};
 </script>
 
 <style scoped>
+p {
+  text-align: justify;
+}
+
+.jumbotron {
+  background-color: white;
+}
 </style>
